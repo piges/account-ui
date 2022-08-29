2ebee30e501790916a497076b7fed6578df5e48a
Cavallaro Catello Stefano <stefano@stepo.biz>
2022-08-29 14:13:02 +0200

    fix missed redirect

--- src/app/modules/piges-account/service/piges-account-auth.service.ts
+++ src/app/modules/piges-account/service/piges-account-auth.service.ts
@@ -1,28 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
 import { APP_URL } from 'src/app/app.constants';
 
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class PigesAccountAuthService {
 
 	constructor(
 		private http: HttpClient,
 		private recaptchaV3Service: ReCaptchaV3Service,
+		private route: ActivatedRoute,
 	) { }
-  
+
 	private configUrl = APP_URL + '/api/login';
 
 	async getIdentityProvider(email: string) {
 		let token = await this.recaptchaV3Service.execute('login').toPromise();
-		
-		return this.http.post<any>(this.configUrl, {
+
+		let queryParamString = this.getQueryParamString();
+
+		return this.http.post<any>(this.configUrl + "?" + queryParamString, {
 			email: email,
 			"g-recaptcha-response": token
 		}).toPromise();
 	}
-	
+
+	getQueryParamString() {
+		let queryParamArray: any[] = [];
+		Object.keys(this.route.snapshot.queryParams).forEach((key, index) => {
+			let value = this.route.snapshot.queryParams[key];
+			if(value != "") {
+				queryParamArray.push(key + "=" + value);
+			}
+		});
+
+		return queryParamArray.join("&");
+	}
+
 }
